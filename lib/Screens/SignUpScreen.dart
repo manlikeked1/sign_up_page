825870e4ae815b1e2b19f23ecb32dbb2b13d7493
manlikeked1 <donaldkene123@gmail.com>
2020-08-25 22:29:00 +0100

    new commit

--- lib/Screens/SignUpScreen.dart
+++ lib/Screens/SignUpScreen.dart
@@ -8,6 +8,164 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: ListView(
+        children: <Widget>[
+          // Here, this widget is responsible for the area that contains the
+          // image and the back button icon
+          BackButtonWidget(),
+
+          //Body of the screen
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.person), onPressed: () {}),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(left: 4, right: 20),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Username'),
+                        )))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.lock), onPressed: () {}),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(left: 4, right: 20),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Password'),
+                        )))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.mail), onPressed: () {}),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(left: 4, right: 20),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Email'),
+                        )))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Radio(value: null, groupValue: null, onChanged: null),
+                RichText(
+                    text: TextSpan(
+                        text: 'I have accepted the  ',
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                      TextSpan(
+                          text: 'Terms& Conditions',
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.bold))
+                    ]))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Container(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.redAccent,
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BackButtonWidget extends StatelessWidget {
+  const BackButtonWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Container responsible for the image at the top
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image:
+                  AssetImage('asset/img/aaron-burden-vKBdY7e7KFk-unsplash.jpg'),
+              fit: BoxFit.cover)),
+      // child: Positioned(
+      //The stack here is placed in order for us to place different widgets on
+      //top of each other
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+              top: 20,
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'SignIn');
+                      }),
+                  Text(
+                    'Back',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )),
+          Positioned(
+            bottom: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Create New Account',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
